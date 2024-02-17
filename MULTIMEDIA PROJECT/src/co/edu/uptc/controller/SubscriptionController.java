d7a877c1cdb4d3fed8fff6fbf48e25963a2a7a54
Laura Urueña <136533030+laurva16@users.noreply.github.com>
2024-02-17 10:27:58 -0500

    Add files via upload

---
+++ MULTIMEDIA PROJECT/src/co/edu/uptc/controller/SubscriptionController.java
@@ -0,0 +1,117 @@
+package co.edu.uptc.controller;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+
+import co.edu.uptc.model.Subscription;
+import co.edu.uptc.model.UserRegistered;
+import co.edu.uptc.model.UserSubscription;
+import co.edu.uptc.persistence.managerClasses.SubscriptionsManagement;
+
+public class SubscriptionController {
+    private SubscriptionsManagement sm = new SubscriptionsManagement();
+    private ArrayList<Subscription> subscriptions = sm.getSubscriptions();
+    private Subscription currentSub;
+
+    public ArrayList<Subscription> getSubscriptions() {
+        return subscriptions;
+    }
+
+    public String[] subsNames(){
+        String[] subsNames = new String[subscriptions.size()];
+        for(int i = 0; i < subscriptions.size(); i++){
+            subsNames[i] = subscriptions.get(i).getName();
+        }
+        return subsNames;
+    }
+
+    public String[] subsAvailablesNames(UserSubscription userSub){
+        ArrayList<String> aux = new ArrayList<>();
+
+        if(userSub != null){
+            for(Subscription s : subscriptions){
+                if(!s.getName().equals(userSub.getName())){
+                    aux.add(s.getName());
+                }
+            }
+            String[] subsNames = new String[aux.size()];
+            for(int i = 0; i < aux.size(); i++){
+                subsNames[i] = aux.get(i);
+            }
+            return subsNames;
+        }else{
+            String[] subsNames = new String[subscriptions.size()];
+            for(int i = 0; i < subscriptions.size(); i++){
+                subsNames[i] = subscriptions.get(i).getName();
+            }
+            return subsNames;
+        }
+    }
+
+    public void addSubToUser(Subscription sub, UserRegistered user){
+        LocalTime currentLocalTime = LocalTime.now(ZoneId.of("America/Bogota"));
+        UserSubscription userSub = new UserSubscription(sub.getName(), sub.getDuration(), sub.getDescription(),
+                         sub.getPrice(), currentLocalTime.toNanoOfDay() / 1_000_000, currentLocalTime.toNanoOfDay() / 1_000_000 + sub.getDuration());
+        user.setSub(userSub);
+    }
+
+    public void cancelSub(UserRegistered user){
+        user.setSub(null);
+    }
+
+    public void findCurrentSubscription(String nameSub){
+        for(Subscription i: subscriptions){
+            if(i.getName().equals(nameSub)){
+                currentSub = i;
+            }
+        }
+    }
+
+
+
+    public void addSubscription(String name, String description, int duration, double price){
+        Subscription newSub = new Subscription(name, duration, description, price);
+        subscriptions.add(newSub);
+        sm.addSubscription(newSub);
+    }
+
+    public void modifySubscription(Subscription subToUpdate, String newName, String newDescription, int newDuration, double newPrice){
+        currentSub.setName(newName);
+        currentSub.setDescription(newDescription);
+        currentSub.setDuration(newDuration);
+        currentSub.setPrice(newPrice);
+
+        sm.updateSubscription(subToUpdate, currentSub);
+    }
+
+    public void removeSubscription(){
+        sm.removeSubscription(currentSub);
+        subscriptions.remove(currentSub);
+    }
+
+    public boolean nameFound(String name){
+        for(Subscription s : subscriptions){
+            if(s.getName().equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Subscription getCurrentSub() {
+        return currentSub;
+    }
+
+    public boolean nameRepeated(String name){
+        for(Subscription s: subscriptions){
+            if(s.getName().equals(name)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+
+    
+}
---
+++ MULTIMEDIA PROJECT/src/co/edu/uptc/model/Person.java
@@ -0,0 +1,59 @@
+package co.edu.uptc.model;
+
+
+public abstract class Person {
+
+    public abstract boolean couldLogIn(String user, String password);
+    
+    private String firstName;
+    private String lastName;
+    private String user;
+    private String password;
+    private int id;
+
+    public Person(){}
+
+    public Person(String firstName, String lastName, int id, String user, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.user = user;
+        this.password = password;
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+}
