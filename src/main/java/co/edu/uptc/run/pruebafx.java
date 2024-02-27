7fa1fe29304239eee6fd7a6d3f0d1b506b4aff46
URUEÑA LAURA <laura.uruena13@uptc.edu.co>
2024-02-27 07:09:39 -0500

    fx

--- src/main/java/co/edu/uptc/run/pruebafx.java
+++ src/main/java/co/edu/uptc/run/pruebafx.java
@@ -10,22 +10,7 @@ public class pruebafx extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Creamos un nuevo objeto Label con el mensaje "Hola, funcionó"
-        Label label = new Label("Hola, funcionó");
 
-        // Creamos un layout StackPane y agregamos el Label
-        StackPane root = new StackPane();
-        root.getChildren().add(label);
-
-        // Creamos la escena y le asignamos el layout
-        Scene scene = new Scene(root, 300, 200);
-
-        // Configuramos el Stage (ventana principal)
-        primaryStage.setTitle("Hola Mundo JavaFX");
-        primaryStage.setScene(scene);
-
-        // Mostramos la ventana
-        primaryStage.show();
     }
 
     public static void main(String[] args) {
