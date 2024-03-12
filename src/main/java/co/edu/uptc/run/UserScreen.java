794f1b1d0ca08565f4218ae558fbeaefa7a81af4
danieldd57 <daniel.vargas19@uptc.edu.co>
2024-03-11 22:30:26 -0500

    changes in user screen

--- src/main/java/co/edu/uptc/run/UserScreen.java
+++ src/main/java/co/edu/uptc/run/UserScreen.java
@@ -402,7 +402,7 @@ public class UserScreen {
         rootPane.setId("root2");
         rootPane.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        ImageView imageView = new ImageView(new Image("file:" + "src\\multimediaCovers\\Series\\stranger.jpeg"));
+        ImageView imageView = new ImageView(new Image("file:" + "src\\multimediaCovers\\Series\\"+ serie.getCoverImage()));
         imageView.setPreserveRatio(true);
         imageView.setFitWidth(820);
         imageView.setFitHeight(400);
